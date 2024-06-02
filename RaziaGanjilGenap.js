e27091301e0ac7f4c68e7f87735e3a11c6769664
Zezcaa <137063519+Zezcaa@users.noreply.github.com>
2024-06-02 23:03:12 +0700

    Update RaziaGanjilGenap.js

--- RaziaGanjilGenap.js
+++ RaziaGanjilGenap.js
@@ -1,5 +1,4 @@
 function kenaRazia(tanggal, data) {
-  // Daftar rute yang kena tilang
   const tilangPerRute = {
     'Gajah Mada': 1,
     'Hayam Wuruk': 1,
@@ -9,76 +8,76 @@ function kenaRazia(tanggal, data) {
     'Tomang Raya': 1
   };
 
-  // Daftar pelanggar
   const pelanggar = [];
 
-  // Proses data pelanggar
-  for (const orang of data) {
-    if (orang.type === "Mobil") {
+  for (let i = 0; i < data.length; i++) {
+    const orang = data[i];
+    if (orang.type === "Mobil" && tanggal >= 27) {
       let jumlahTilang = 0;
-      for (const rute of orang.rute) {
+      for (let j = 0; j < orang.rute.length; j++) {
+        const rute = orang.rute[j];
         if (tilangPerRute[rute]) {
           jumlahTilang += tilangPerRute[rute];
         }
       }
 
-      if (jumlahTilang > 0 && tanggal >= 27) {
-        const existingPelanggar = pelanggar.find(p => p.name === orang.name);
-        if (existingPelanggar) {
-          existingPelanggar.tilang += jumlahTilang;
-        } else {
-          pelanggar.push({ name: orang.name, tilang: jumlahTilang });
+      let existingPelanggar = null;
+      for (let k = 0; k < pelanggar.length; k++) {
+        if (pelanggar[k].name === orang.name) {
+          existingPelanggar = pelanggar[k];
+          break;
         }
+      }
+
+      if (existingPelanggar) {
+        existingPelanggar.tilang += jumlahTilang;
+      } else {
+        pelanggar.push({ name: orang.name, tilang: jumlahTilang });
       }
     }
   }
 
-  // Kembalikan daftar pelanggar
   return pelanggar;
 }
 
-// Contoh penggunaan function
-const dataPelanggar = [
-  {
-    name: "Denver",
-    plat: "B 2791 KDS",
-    type: "Mobil",
-    rute: ["TB Simatupang", "Panglima Polim", "Depok", "Senen Raya"]
-  },
-  {
-    name: "Toni",
-    plat: "B 1212 JBB",
-    type: "Mobil",
-    rute: [
-      "Pintu Besar Selatan",
-      "Panglima Polim",
-      "Depok",
-      "Senen Raya",
-      "Kemang"
-    ]
-  },
-  {
-    name: "Stark",
-    plat: "B 444 XSX",
-    type: "Motor",
-    rute: ["Pondok Indah", "Depok", "Senen Raya", "Kemang"]
-  },
-  {
-    name: "Anna",
-    plat: "B 678 DD",
-    type: "Mobil",
-    rute: [
-      "Fatmawati",
-      "Panglima Polim",
-      "Depok",
-      "Senen Raya",
-      "Kemang",
-      "Gajah Mada"
-    ]
-  }
-];
-
-const tanggalRazia = 27;
-
-const pelanggar = kenaRazia(tanggalRazia, dataPelanggar);
-console.log(pelanggar);
+console.log(
+  kenaRazia(27, [
+    {
+      name: "Denver",
+      plat: "B 2791 KDS",
+      type: "Mobil",
+      rute: ["TB Simatupang", "Panglima Polim", "Depok", "Senen Raya"]
+    },
+    {
+      name: "Toni",
+      plat: "B 1212 JBB",
+      type: "Mobil",
+      rute: [
+        "Pintu Besar Selatan",
+        "Panglima Polim",
+        "Depok",
+        "Senen Raya",
+        "Kemang"
+      ]
+    },
+    {
+      name: "Stark",
+      plat: "B 444 XSX",
+      type: "Motor",
+      rute: ["Pondok Indah", "Depok", "Senen Raya", "Kemang"]
+    },
+    {
+      name: "Anna",
+      plat: "B 678 DD",
+      type: "Mobil",
+      rute: [
+        "Fatmawati",
+        "Panglima Polim",
+        "Depok",
+        "Senen Raya",
+        "Kemang",
+        "Gajah Mada"
+      ]
+    }
+  ])
+);
